7e82c479c0b725d7740bcef0a1badf7766aa270b
ImExtraPolite <imextrapolite@gmail.com>
2023-12-31 19:21:48 -0500

    finish making rps game

--- index.js
+++ index.js
@@ -20,22 +20,47 @@ const playGame = (playerSelection, computerSelection) => {
   if (playerSelection === 'rock' && computerSelection === 'rock') {
     return 'tie game';
   } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-    return 'user wins';
+    return 'Computer chose paper. Computer wins';
   } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-    return 'computer wins';
+    return 'Computer chose scissors. User wins';
   } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-    return 'user wins';
+    return 'Computer chose rock. User wins';
   } else if (playerSelection === 'paper' && computerSelection === 'paper') {
     return 'tie game';
   } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-    return 'computer wins';
+    return 'Computer chose scissors. Computer wins';
   } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-    return 'computer wins';
+    return 'Computer chose rock. Computer wins';
   } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-    return 'user wins';
+    return 'Computer chose paper. User wins';
   } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
     return 'tie game';
   }
 }
 
-console.log(playGame(getPlayerChoice(), getComputerChoice()));
+const game = () => {
+  let round = playGame(getPlayerChoice(), getComputerChoice());
+  console.log(round);
+
+  if (round === 'Computer chose scissors. User wins' || round === 'Computer chose rock. User wins' || round === 'Computer chose paper. User wins') {
+    wins++;
+    // console.log(wins);
+  } else if (round === 'Computer chose paper. Computer wins' || round === 'Computer chose scissors. Computer wins' || round === 'Computer chose rock. Computer wins') {
+    loses++;
+  }
+
+  if (wins === 5) {
+    return 'user wins the game';
+  } else if (loses === 5) {
+    return 'computer wins the game';
+  }
+}
+
+let wins = 0;
+let loses = 0;
+
+while (wins < 5 && loses < 5) {
+  game();
+}
+
+console.log(game());
